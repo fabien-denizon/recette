3faa65140ae7bd3eefa5625ab9e8d359dd1694f1
ikit <ikit@blackdragon.fr>
2018-10-12 11:18:44 +0200

    adding functions to the project

---
+++ app/src/main/java/com/example/ikit/recette/AppWebViewClient.java
@@ -0,0 +1,4 @@
+package com.example.ikit.recette;
+
+public class AppWebViewClient {
+}
---
+++ app/src/main/java/com/example/ikit/recette/TechniquesAjouterActivity.java
@@ -0,0 +1,12 @@
+package com.example.ikit.recette;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TechniqueAjouterActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+    }
+}
